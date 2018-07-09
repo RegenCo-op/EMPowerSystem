c67bd4670991428762500b9127c3758a1cd8a74a
Bryce Hennen <ecyrbnenneh@gmail.com>
2018-07-08 19:55:32 -0500

    make tank level sensor checks. still need dtc handling

--- Libraries/ThermalFluidController/src/TankLevelSensors/TankLevelSensors.cpp
+++ Libraries/ThermalFluidController/src/TankLevelSensors/TankLevelSensors.cpp
@@ -24,4 +24,126 @@ int TankLevelSensors::ReadSensor2Level_()
 	return sensor_2_level_;
 }
 
+bool TankLevelSensors::CheckTankLevelPlausibility()
+{
+	sensor_1_is_plausible_ = false;
+	sensor_2_is_plausible_ = false;
 
+	ReadSensor1Level_();
+	ReadSensor2Level_();
+
+	if (sensor_1_level_ > plausible_level_) //plausible level is only the min value, since ping_cm returns 0 if reading > max distance
+	{
+		sensor_1_is_plausible_ = true;
+		//TODO: set dtc
+	}
+
+	if (sensor_2_level_ > plausible_level_)
+	{
+		sensor_2_is_plausible_ = true;
+		//TODO: set dtc
+	}
+
+	if(!sensor_1_is_plausible_ && !sensor_2_is_plausible_)
+	{
+		//TODO: set dtc for both failing and exit check
+		return false;
+	}
+
+	//TODO: maybe increment failure counter
+	return true;
+}
+
+bool TankLevelSensors::CheckTankLevelSensorDifferential()
+{
+	int diff = sensor_1_level_ - sensor_2_level_;
+	if (abs(diff) > level_diff_spec_range_)
+	{
+		//differential is too large
+		//TODO: set dtc
+		//TODO: maybe increment failure counter
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+bool TankLevelSensors::CheckTankLevel(bool pump_on)
+{
+	//set calibration level depending on pump state
+	int calibration_level;
+	if (pump_on)
+	{
+		//compare tank level with the calibrated level with the pump on
+		calibration_level = full_level_pump_on_;
+	}
+	else
+	{
+		//compare tank level with the calibrated level with the pump off
+		calibration_level = full_level_pump_off_;
+	}
+
+	//compare tank level to calibrated level
+	int diff = tank_level_ - calibration_level;
+	if (abs(diff) > tank_level_max_error_)
+	{
+		//differential is too large
+		//TODO: set dtc
+		//TODO: maybe increment failure counter
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+bool TankLevelSensors::CheckTankLevelSensors(bool pump_on)
+{
+	if (!CheckTankLevelPlausibility())
+	{
+		//Both sensors were implausible, don't continue checking
+		return false;
+	}
+
+	//Get tank level
+	if (sensor_1_is_plausible_ && sensor_2_is_plausible_)
+	{
+		//both sensors plausible
+		if (CheckTankLevelSensorDifferential())
+		{
+			//differential is within limits
+			tank_level_ = (sensor_1_level_ + sensor_2_level_) / 2;
+		}
+		else
+		{
+			//differential is out of limits, use lower level sensor
+			if (sensor_1_level_ < sensor_2_level_)
+			{
+				tank_level_ = sensor_1_level_;
+			}
+			else
+			{
+				tank_level_ = sensor_2_level_;
+			}
+		}
+	}
+	else //One of the sensor readings are not plausible
+	{
+		//TODO: maybe increment failure counter
+		//Set tank level to be the one plausible sensor level
+		if (sensor_1_is_plausible_)
+		{
+			tank_level_ = sensor_1_level_;
+		}
+		else
+		{
+			tank_level_ = sensor_2_level_;
+		}
+	}
+
+	//compare tank level to calibrated level
+	return CheckTankLevel(pump_on);
+}
--- Libraries/ThermalFluidController/src/TankLevelSensors/TankLevelSensors.h
+++ Libraries/ThermalFluidController/src/TankLevelSensors/TankLevelSensors.h
@@ -10,6 +10,7 @@ struct TankLevelSensorsConfig
 	int plausible_tank_level_max;
 	int level_diff_spec_range;
 	int full_level_pump_off;
+	int full_level_pump_on;
 	int tank_level_max_error;
 };
 
@@ -20,21 +21,38 @@ private:
 	NewPing sensor_2_;
 	int sensor_1_level_;
 	int sensor_2_level_;
-	int level_;
-	bool half_fail_ = true;
-	bool full_fail_ = true;
+	int tank_level_;
 
 	int plausible_level_;
 	int level_diff_spec_range_;
 	int full_level_pump_off_;
+	int full_level_pump_on_;
 	int tank_level_max_error_;
+
+	bool sensor_1_is_plausible_;
+	bool sensor_2_is_plausible_;
 
 	int ReadSensor1Level_();
 	int ReadSensor2Level_();
 
+
+	bool CheckTankLevelPlausibility();
+	bool CheckTankLevelSensorDifferential();
+	bool CheckTankLevel(bool pump_on);
+
 public:
 	TankLevelSensors(TankLevelSensorsConfig tank_level_sensors_config);
 
+	//Checks tank level using the ultrasonic sensors.
+	//Checks if the sensors are plausible, if both implausible
+	//returns false. If only one plausible, uses plausible sensor
+	//for tank level. If both plausible, compares
+	//sensors to see if they are reading close to the same
+	//level. If they are close averages the two values for the tank level,
+	//otherwise uses the lower reading to set the tank level.
+	//Checks to see if tank level is correct for operating conditions.
+	//If correct returns true, otherwise returns false.
+	bool CheckTankLevelSensors(bool pump_on);
 };
 
 #endif
